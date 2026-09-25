5a62d0c83d8972e984072a2a11caa9c543ff3135
13098559235 <59509295+13098559235@users.noreply.github.com>
2026-09-25 21:53:50 +0800

    Add files via upload

--- diy‑part1.sh
+++ diy‑part1.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
-echo "CONFIG_SMALL_FLASH=y" >> .config
-echo "CONFIG_TARGET_ROOTFS_PARTSIZE=6700" >> .config
+# Copyright (c) 2019‑2020 P3TERX <https://p3terx.com>
+# Actions‑OpenWrt diy‑part1.sh
+# 执行时机：更新 feeds 之前
+# 方案2：LF‑R190 AR9331 8M/64M，禁止任何内核动态patch，防止DTS损坏
 
-# 单块网卡最大8虚拟AP，支持STA+AP边收边发
-echo "CONFIG_WIRELESS_MAX_VAPS=8" >> .config
+# ========= 不添加任何第三方feed，使用源码自带官方feed =========
+# 这里不做wget/patch，彻底关闭内核打补丁，避免#24故障
 
-# LuCI完整中文界面
-echo "CONFIG_LUCI_LANG_zh-cn=y" >> .config
-
-# USB挂卡驱动（RT3070归属rt2800‑usb）
-echo "CONFIG_PACKAGE_kmod-net-rtl8188-usb=y" >> .config
-echo "CONFIG_PACKAGE_kmod-net-rtl8192cu=y" >> .config
-echo "CONFIG_PACKAGE_kmod-rt2800-usb=y" >> .config
-echo "CONFIG_PACKAGE_kmod-mt7601u=y" >> .config
-echo "CONFIG_PACKAGE_kmod-mt76-usb=y" >> .config
-
-# 在线设备管控：网页点击拉黑，无需输入MAC
-echo "CONFIG_PACKAGE_luci-app-online-users=y" >> .config
-
-# WDS桥接、relayd智能中继全套底层组件
-echo "CONFIG_PACKAGE_relayd=y" >> .config
-echo "CONFIG_PACKAGE_luci-proto-relay=y" >> .config
-
-# Portal网页认证底层依赖库，只编译库，主程序刷机后在线安装
-echo "CONFIG_PACKAGE_luci-lib-portal=y" >> .config
-
-echo "diy-part1 run ok"
+echo "==== diy‑part1.sh done ===="
